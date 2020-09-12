6b0078d96f41444d16dd90ea2efac9be61a01a30
Nahum Smith <nahumsmith@gmail.com>
2020-09-12 09:34:11 -0400

    Organize Codebase and add better error checking and logs for Cloudwatch
    separate modules for individual functions
    spread some more error checks and logs around for debugging purposes.  [Todo] :  create a logging class to tie into stages

---
+++ functions/analyzeAudio.js
@@ -0,0 +1,15 @@
+const { transcribeAudioFromS3 } = require("../utils/utils.js");
+
+module.exports.default = async (event) => {
+  try {
+    const res = await transcribeAudioFromS3(
+      `https://${process.env.audioInputBucket}.s3.amazonaws.com/${process.env.audioFile}`,
+      process.env.transcriptionJobName,
+      process.env.audioOutputBucket
+    );
+    // Log for Debugging in Cloudwatch
+    console.log("TRANSCRIBE RESULTS: ", JSON.stringify(res));
+  } catch (err) {
+    console.log("TRANSCRIBE ERROR: ", JSON.stringify(err));
+  }
+};
